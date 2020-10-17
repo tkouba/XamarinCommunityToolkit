5c8e8f4189216902bd7b523d8b385e3d626f3c46
Steven Thewissen <github@thewissen.io>
2020-10-17 10:17:14 +0200

    Moved converter + beginnings of a sample

--- XamarinCommunityToolkitSample/ViewModels/Views/ViewsGalleryViewModel.cs
+++ XamarinCommunityToolkitSample/ViewModels/Views/ViewsGalleryViewModel.cs
@@ -17,8 +17,7 @@ namespace Xamarin.CommunityToolkit.Sample.ViewModels.Views
 			new SectionModel(typeof(ExpanderPage), AppResources.ExpanderTitle, AppResources.ExpanderDescription),
 			new SectionModel(typeof(ActionsPage), AppResources.ActionsPageTitle, AppResources.ActionsPageDescription),
 			new SectionModel(typeof(MediaElementPage), AppResources.MediaElementTitle, AppResources.MediaElementDescription),
-			new SectionModel(typeof(RangeSliderPage), AppResources.RangeSliderTitle, AppResources.RangeSliderDescription),
-			new SectionModel(typeof(SideMenuViewPage), AppResources.SideMenuViewTitle, AppResources.SideMenuViewDescription)
+			new SectionModel(typeof(StateLayoutPage), AppResources.SideMenuViewTitle, AppResources.SideMenuViewDescription)
 		};
 	}
 }
